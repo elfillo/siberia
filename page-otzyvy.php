5df4b08f8e6424f6181b789423112f31951f5dbd
elf <filonenko0406@gmail.com>
2019-07-27 14:17:26 +0800

    fix reviews

--- page-otzyvy.php
+++ page-otzyvy.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <section class="textSection">
         <div class="title">С момента создания АНО “Здоровая Сибирь” мы смогли помочь многим людям, вот не большая часть людей которая согласилась написать об этом пару слов</div>
-        <?php if (have_posts()): query_posts('category_name=comments'); while (have_posts()): the_post(); ?>
+        <?php if (have_posts()): query_posts('category_name=reviews'); while (have_posts()): the_post(); ?>
             <div class="review_block">
                 <div class="text"><?php the_content()?></div>
                 <div class="name"><?php the_title()?></div>
